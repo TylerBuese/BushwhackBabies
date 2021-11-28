7ed7152c49a95fbed87173fbd92f53bb6f7565e6
bzb0y <tylerbuese@gmail.com>
2021-11-27 18:40:34 -0600

    Added shooting mechainc

--- main.cpp
+++ main.cpp
@@ -5,35 +5,20 @@
         Shoot alligators in the sky, if they hit the ground you lose a point. if you hit them, you gain a point
 */
 
-class Bullet
-{
-    public:
-    void DrawBullet(int currX, int currY)
-    {
-        int bullety {currY}; 
-        DrawRectangle(currX, bullety, 5, 20, RED);
-    }
-
-    private:
-    Rectangle Bullets[100] {0,0,0,0};
-};
-
 int main() 
 {
     InitWindow(1280, 720, "Bushwhack Babies");
     Rectangle enemy {200, 200, 50, 50};
     Rectangle baby {600, 1280 / 2 - baby.width, 50, 25};
+    Rectangle bullet[10000] {0, 0, 5, 20};
 
     float gravity {100};
     bool gameOverWin {false};
     bool gameOverLose {false};
     bool bulletFired {false};
-    int numberOfBullets {0};
     int counter {0};
+    int bulletNum {0};
     float time {0};
-    int bullety {baby.y};
-    int bulletx {baby.x};
-    bool gotVars {false};
     SetTargetFPS(60);
     
 
@@ -48,14 +33,25 @@ int main()
         time = GetTime();
 
         //check collision
-        bool onEnemy {false};
-        if (!gameOverWin || !gameOverLose)
+        // bool onEnemy {false};
+        // if (!gameOverWin || !gameOverLose)
+        // {
+        //     if (GetMouseX() <= enemy.x + enemy.width &&
+        //         GetMouseX() >=enemy.x &&
+        //         GetMouseY() <= enemy.y + enemy.height &&
+        //         GetMouseY() >= enemy.y)
+        //         onEnemy = true;
+        // }
+
+        for (int i = 0; i <= bulletNum; i++)
         {
-            if (GetMouseX() <= enemy.x + enemy.width &&
-                GetMouseX() >=enemy.x &&
-                GetMouseY() <= enemy.y + enemy.height &&
-                GetMouseY() >= enemy.y)
-                onEnemy = true;
+            if (CheckCollisionRecs(bullet[i], enemy))
+            {
+                enemy.x = GetRandomValue(100, 1100);
+                enemy.y = GetRandomValue(0, 100);
+                counter++;
+                gravity *= 1.125;
+            }
         }
 
         if (CheckCollisionRecs(enemy, baby) || enemy.y >= GetScreenHeight())
@@ -66,49 +62,37 @@ int main()
         enemy.y += gravity * GetFrameTime();
 
         if (IsKeyDown(KEY_A))
-            baby.x -= 100 * GetFrameTime();
+            baby.x -= 750 * GetFrameTime();
         
 
         if (IsKeyDown(KEY_D))
-            baby.x += 100 * GetFrameTime();
+            baby.x += 750 * GetFrameTime();
+            
 
         if (IsKeyPressed(KEY_SPACE))
         {
             //fire bullet
             bulletFired = true;
-            numberOfBullets++;
-            
-            
-
+            bulletNum += 1;
+            bullet[bulletNum].y = baby.y;
+            bullet[bulletNum].x = baby.x + 20;
+            bullet[bulletNum].width = 5;
+            bullet[bulletNum].height = 20;
         }
 
         if (bulletFired)
-        {
-            // DrawRectangle(bulletx + baby.width / 2, bullety, 5, 20, RED);
-            // bullety -= 100 * GetFrameTime();
-            Bullet bullet;
-
-            //get vars
-            if (!gotVars)
+        {   
+            for (int i = 0; i <= bulletNum; i++)
             {
-                
-                bulletx = baby.x;
-                gotVars = true;
+                bullet[i].y -= 500 * GetFrameTime();
+                DrawRectangleRec(bullet[i], WHITE);
             }
-            bullety -= 10 * GetFrameTime();
-            bullet.DrawBullet(bulletx, bullety);
-            
-            DrawText("Fired!", 100, 100, 10, RED);
             
         }
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && onEnemy)
+
+        if (counter == 100)
         {
-            enemy.x = GetRandomValue(100, 1100);
-            enemy.y = GetRandomValue(0, 100);
-            counter++;
-            gravity *= 1.125;
-            bullety = baby.y;
-            bulletFired = true;
+            gameOverWin = true;
         }
 
         if (gameOverWin)
@@ -128,15 +112,5 @@ int main()
 
         EndDrawing();
     }
-
-  
-
-
-
     return 0;
-}
-
-void fireBullet(int x, int y)
-{
-
 }
